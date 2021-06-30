f4c1f75f5fd2f784b7f77bdbc64b43cb6172086d
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-06-30 16:38:49 -0300

    Anotação personalizada para registro existente ok

--- casadocodigo/src/main/java/com/casadocodigo/casadocodigo/controller/form/CategoriaForm.java
+++ casadocodigo/src/main/java/com/casadocodigo/casadocodigo/controller/form/CategoriaForm.java
@@ -3,6 +3,7 @@ package com.casadocodigo.casadocodigo.controller.form;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import com.casadocodigo.casadocodigo.config.validacao.UniqueValue;
 import com.casadocodigo.casadocodigo.modelo.Categoria;
 
 import org.hibernate.validator.constraints.Length;
@@ -10,6 +11,7 @@ import org.hibernate.validator.constraints.Length;
 public class CategoriaForm {
 
     @NotNull @NotEmpty @Length(max = 40)
+    @UniqueValue(domainClass = Categoria.class, fieldName = "nomeCategoria")
     private String nomeCategoria;
 
     public CategoriaForm() {
